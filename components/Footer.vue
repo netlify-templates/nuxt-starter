e21f93a3901487711c0a0fa5fcbbe152b4ccd87e
Vin Rocco <119914016+CreativLogic@users.noreply.github.com>
2025-07-16 15:27:07 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="my-20">
     <p class="text-sm text-center text-zinc-600">
-      Copyright &copy; {{ new Date().getFullYear() }} Nuxt Starter. Powered by <a href="https://www.netlify.com/" class="underline">Netlify</a>.
+      Copyright &copy; {{ new Date().getFullYear() }} ClickStorm Media Powered by <a href="https://www.netlify.com/" class="underline">NoCap Copy </a>.
     </p>
   </footer>
 </template>
